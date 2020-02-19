95f276b1fa7ccbf460915b7549d86623981d453f
Lucas Cavalcante da Silva <silva.lucasdev@gmail.com>
2020-02-19 17:40:38 -0300

    Setup a blank Main Window

--- imageviewer/app.py
+++ imageviewer/app.py
@@ -1,3 +1,13 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication
+
 from imageviewer.view import MainWindow
 
-MainWindow()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec_())
--- imageviewer/view/main_window.py
+++ imageviewer/view/main_window.py
@@ -1,2 +1,15 @@
-class MainWindow:
-    pass
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtGui import QGuiApplication
+
+from imageviewer import __app_name__
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+
+        self.setupUi()
+
+    def setupUi(self):
+        self.setWindowTitle(__app_name__)
+        self.resize(QGuiApplication.primaryScreen().availableSize() * 2 / 3)
