26d126ed16d47a936105a4d70f55100b5d01b033
Lucas Cavalcante da Silva <silva.lucasdev@gmail.com>
2020-02-19 19:35:07 -0300

    Made initial application UI.

--- imageviewer/app.py
+++ imageviewer/app.py
@@ -2,10 +2,12 @@ import sys
 
 from PyQt5.QtWidgets import QApplication
 
+from imageviewer import __app_name__, __version__
 from imageviewer.view import MainWindow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setApplicationName(__app_name__)
 
     window = MainWindow()
     window.show()
--- imageviewer/view/main_window.py
+++ imageviewer/view/main_window.py
@@ -1,15 +1,69 @@
-from PyQt5.QtWidgets import QMainWindow
-from PyQt5.QtGui import QGuiApplication
-
-from imageviewer import __app_name__
+from PyQt5.QtWidgets import (QMainWindow, QLabel, QPushButton, QVBoxLayout,
+                             QHBoxLayout, QFrame, QSizePolicy, QWidget,
+                             QAction)
+from PyQt5.QtGui import QGuiApplication, QPalette
+from PyQt5.QtCore import Qt
 
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
 
+        self.setupMenuBar()
         self.setupUi()
 
     def setupUi(self):
-        self.setWindowTitle(__app_name__)
         self.resize(QGuiApplication.primaryScreen().availableSize() * 2 / 3)
+
+        # Create a label to display the images.
+        self.imageLabel = QLabel(
+            "Press Ctrl+O or click on 'File' to select an image!")
+        self.imageLabel.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
+        self.imageLabel.setBackgroundRole(QPalette.Base)
+        self.imageLabel.setAlignment(Qt.AlignCenter)
+
+        # Create the index area.
+        # Button - Text - Button
+
+        # A frame to draw a line around the layout.
+        frame = QFrame()
+        frame.setFrameShadow(QFrame.Raised)
+        frame.setFrameShape(QFrame.StyledPanel)
+        frame.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
+
+        self.buttonPrevious = QPushButton("Previous")
+        self.buttonPrevious.setMinimumSize(100, 40)
+        self.buttonPrevious.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+
+        self.labelIndex = QLabel()
+        self.labelIndex.setAlignment(Qt.AlignCenter)
+
+        self.buttonNext = QPushButton("Next")
+        self.buttonNext.setMinimumSize(100, 40)
+        self.buttonNext.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+
+        frameLayout = QHBoxLayout()
+        frameLayout.addWidget(self.buttonPrevious)
+        frameLayout.addWidget(self.labelIndex)
+        frameLayout.addWidget(self.buttonNext)
+
+        frame.setLayout(frameLayout)
+
+        # Create and add widgets to the main layout.
+        mainLayout = QVBoxLayout()
+        mainLayout.addWidget(self.imageLabel)
+        mainLayout.addWidget(frame)
+
+        # Create central widget.
+        centralWidget = QWidget()
+        centralWidget.setLayout(mainLayout)
+        self.setCentralWidget(centralWidget)
+
+    def setupMenuBar(self):
+        openFileAct = QAction("&Open", self)
+        openFileAct.setShortcut("Ctrl+O")
+        openFileAct.setToolTip("Select an image")
+
+        menuBar = self.menuBar()
+        fileMenu = menuBar.addMenu("&File")
+        fileMenu.addAction(openFileAct)
